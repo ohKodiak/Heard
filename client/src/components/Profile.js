3a9dab584f389150fc8618ef4363f8973bcca9ee
ee <77172196+ohKodiak@users.noreply.github.com>
2024-01-11 09:56:20 -0600

    changed up profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,32 +1,32 @@
-import '../styles/Profile.css'
 import React from 'react';
+import '../styles/Profile.css';
 
 function Profile() {
-    return (
-        <div className="profile">
-            <div className="profile-head">
-                <img src="https://via.placeholder.com/50" alt="User" />
-                <div className="profile-info">
-                    <p>Johnny Manziel</p>
-                    <div className="profile-info-extra">
-                        <p>2012 Friends</p>
-                        <p>Joined 07/24/1989</p>
-                    </div>
-                </div>
-            </div>
-            <div className="profile-body">
-                <div className="profile-fav-artists">
-                    <p>Favorite Artists</p>
-                </div>
-                <div className="profile-fav-songs">
-                    <p>Favorite Songs</p>
-                </div>
-            </div>
-            <div className="profile-footer">
-                <p>Friends</p>
-            </div>
+  return (
+    <div className="profile">
+      <div className="profile-top">
+        <div className="profile-picture">
+          <img src="https://via.placeholder.com/200" alt="User" />
         </div>
-    );
-};
+        <div className="profile-info">
+          <p className="profile-info-name">Johnny Manziel</p>
+          <div className="profile-info-extra">
+          <p className="profile-info-friends">2012 Friends</p>
+          <p className="profile-info-joined">Joined 07/24/1989</p>
+          </div>
+        </div>
+      </div>
+      <div className="profile-middle">
+        <div className="profile-fav">
+          <p className="profile-fav-artists">Favorite Artists</p>
+          <p className="profile-fav-songs">Favorite Songs</p>
+        </div>
+      </div>
+      <div className="profile-bottom">
+        <p className="profile-footer">Friends</p>
+      </div>
+    </div>
+  );
+}
 
 export default Profile;
